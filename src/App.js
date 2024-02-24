d8b84ab85b1450ffa0a6d829ca0efbeab6461928
Ajay <108525071+Ajayvadadre@users.noreply.github.com>
2024-02-24 22:52:40 +0530

    changed errors

--- src/App.js
+++ src/App.js
@@ -22,11 +22,7 @@ function App() {
         <Route path='/Men' element={<Categories category='men'/>} />
         <Route path='/Women' element={<Categories category='women'/>} />
         <Route path='/Kids' element={<Categories category='Kids'/>} />
-<<<<<<< HEAD
         <Route path='/Accessories' element={<Categories category='accessories'/>} />
-=======
-        <Route path='/Accessories' element={<Categories category='Accessories'/>} />
->>>>>>> ba7e2edc501c31524250fd29cc6b8f38e6eb3893
         <Route path='/Product/:category/:ProductId' element={ <Product/>}></Route>
         <Route path='/Cart' element={<Cart/>} />
         <Route path='/Login' element={<Login/>} />
--- src/Components/Items/items.jsx
+++ src/Components/Items/items.jsx
@@ -7,13 +7,9 @@ const items = (props) => {
     <>
        <div className="main2">
           <div className="item">
-<<<<<<< HEAD
             <Link to={`/Product/${props.category}/${props.id}`} >
                <img className="product-image" src={props.image} alt="" onClick={window.scrollTo(0,0)} />
             </Link>
-=======
-            <Link to={`/Product/${props.category}/${props.id}`} > <img className="product-image" src={props.image} alt="" onClick={window.scrollTo(0,0)} /></Link>
->>>>>>> ba7e2edc501c31524250fd29cc6b8f38e6eb3893
             <p>{props.name}</p>
             <div className="item-prices">
               <div className="item-price-new">&#x20B9; {props.new_price}</div>
